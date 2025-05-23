839410fd3302d947399c8e4f8236e945b6cc7f56
ThaiHaNGUYEN <s224082764@deakin.edu.au>
2025-05-23 22:15:13 +1000

    12 commit

--- backend/todo.test.js
+++ backend/todo.test.js
@@ -1,8 +1,8 @@
-describe('Always Passing Test', () => {
-  test('true is true', () => {
-    expect(true).toBe(true); // ✅ This will always pass
-  });
-});
+// describe('Always Passing Test', () => {
+//   test('true is true', () => {
+//     expect(true).toBe(true); // ✅ This will always pass
+//   });
+// });
 
 describe('Always Failing Test', () => {
   test('false is true', () => {
