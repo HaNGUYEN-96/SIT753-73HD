eea40eb11fce16b768179e6cbb881ee71ea62715
ThaiHaNGUYEN <s224082764@deakin.edu.au>
2025-05-23 20:20:41 +1000

    firstcomit

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const Todo = require("./models/Todo");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://mongo:27017/todo", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.get("/todos", async (req, res) => {
+    const todos = await Todo.find();
+    res.json(todos);
+});
+
+app.post("/todos", async (req, res) => {
+    const todo = new Todo({ text: req.body.text });
+    await todo.save();
+    res.json(todo);
+});
+
+app.delete("/todos/:id", async (req, res) => {
+    await Todo.findByIdAndDelete(req.params.id);
+    res.sendStatus(204);
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
---
+++ frontend/src/App.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/todos").then(res => setTodos(res.data));
+  }, []);
+
+  const addTodo = () => {
+    axios.post("http://localhost:5000/todos", { text }).then(res => {
+      setTodos([...todos, res.data]);
+      setText("");
+    });
+  };
+
+  const deleteTodo = id => {
+    axios.delete(`http://localhost:5000/todos/${id}`).then(() => {
+      setTodos(todos.filter(todo => todo._id !== id));
+    });
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input value={text} onChange={e => setText(e.target.value)} />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo._id}>
+            {todo.text} <button onClick={() => deleteTodo(todo._id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
