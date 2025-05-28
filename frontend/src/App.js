43c1c5e41a374ebb1a4219c08dfdf4c59861036a
ThaiHaNGUYEN <s224082764@deakin.edu.au>
2025-05-28 22:55:26 +1000

    28th run

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-require('newrelic');
+import axios from "axios"; 
 
 function App() {
   const [todos, setTodos] = useState([]);
